8215b9e2b0145a202779bb0a0d7216b18b7afd7e
7revor <7revor233@gmail.com>
2025-12-16 14:32:37 +0800

    feat: 优化布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Button, Input, message, Radio } from "antd";
 
@@ -7,36 +7,46 @@ interface Option {
   value: "custom" | "delSpace" | "reverse" | "trim";
   code: string;
 }
+const commonSplit = `input.split(/\\r?\\n/)`;
+const commonJoin = `.join("\\n")`;
 const options: Option[] = [
   {
     label: "去空格",
     value: "delSpace",
-    code: "list.map(item => item.replace(/\\s/g, ''))",
+    code: ".map(item => item.replace(/\\s/g, ''))",
   },
   {
     label: "去首尾空格",
     value: "trim",
-    code: "list.map(item => item.trim())",
+    code: ".map(item => item.trim())",
   },
   {
     label: "取反",
     value: "reverse",
-    code: "list.map(item => isNaN(item) ? item : Number(item) * -1)",
+    code: ".map(item => isNaN(item) ? item : Number(item) * -1)",
   },
   {
     label: "自定义",
     value: "custom",
-    code: "list.map(item => item)",
+    code: ".map(item => item)",
   },
 ];
 
 const App = () => {
   const [input, setInput] = useState("");
   const [opt, setOpt] = useState<Option["value"]>("delSpace");
-  const [code, setCode] = useState(options.find((item) => item.value === opt)?.code || "");
+  const [customCode, setCustomCode] = useState("");
   const [result, setResult] = useState("");
+  useEffect(() => {
+    const optCode = options.find((item) => item.value === opt)?.code;
+    if (optCode) {
+      setCustomCode(commonSplit + optCode + commonJoin);
+    } else {
+      setCustomCode("");
+    }
+  }, [opt]);
   const onGenerate = () => {
-    if (!code) {
+    if (!customCode) {
       message.error("请选择操作或输入自定义代码");
       return;
     }
@@ -44,15 +54,9 @@ const App = () => {
       message.error("请输入内容");
       return;
     }
-    const list = input.split(/\r?\n/);
-    if (!list.length) {
-      message.error("请输入内容");
-      return;
-    }
     try {
       // eslint-disable-next-line react-hooks/unsupported-syntax
-      const r = eval(code);
-      const str = r.join("\n");
+      const str = eval(customCode);
       setResult(str);
       navigator.clipboard.writeText(str);
       message.success("结果已复制到剪切板");
@@ -74,24 +78,28 @@ const App = () => {
             onChange={(e) => setInput(e.target.value)}
           />
         </div>
+
         <div className="opt-container">
-          <Radio.Group
-            options={options}
-            defaultValue={opt}
-            optionType="button"
-            onChange={(e) => {
-              setOpt(e.target.value);
-              setCode(options.find((item) => item.value === e.target.value)?.code || "");
-            }}
-          />
-          <Input.TextArea className="input-textarea" rows={10} value={code} onChange={(e) => setCode(e.target.value)} />
+          <div className="opt-title">输出</div>
+          <Input.TextArea className="input-textarea" rows={10} value={result} />
         </div>
       </div>
-      <div className="opt-container">
-        <Button type="primary" style={{ marginTop: "24px" }} onClick={onGenerate}>
+      <div className="opt-container" style={{ marginTop: "24px" }}>
+        <Radio.Group
+          options={options}
+          defaultValue={opt}
+          optionType="button"
+          onChange={(e) => setOpt(e.target.value)}
+        />
+        <Input.TextArea
+          className="input-textarea code-font"
+          rows={10}
+          value={customCode}
+          onChange={(e) => setCustomCode(e.target.value)}
+        />
+        <Button type="primary" style={{ marginTop: "12px" }} onClick={onGenerate}>
           生成
         </Button>
-        <Input.TextArea className="input-textarea" rows={10} value={result} />
       </div>
     </div>
   );
